1120d0cf26eb4155c7486f5e7c6f35a440064aea
evej123 <90426402+evej123@users.noreply.github.com>
2021-09-12 11:31:07 -0500

    Add files via upload

---
+++ EjerciciosSemana3/src/ejerciciossemana3/Mundo.java
@@ -0,0 +1,16 @@
+
+package EjerciciosSemana3;
+
+import javax.swing.JOptionPane;
+
+
+public class Mundo {
+
+    
+    public static void main(String[] args) {
+      JOptionPane.showMessageDialog(null,"Hola mundo1");
+      
+        System.out.println("hola mundo2");
+    }
+    
+}
